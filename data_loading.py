9c05a79d7db1d233ed109cd13fa76940775593fa
Jasna Stankovic <jasnaa994@gmail.com>
2018-01-16 23:05:16 +0100

    Add tokenizing, stop words and lemmatization.

--- data_loading.py
+++ data_loading.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import numpy as np
+import itertools
+
+import matplotlib.pyplot as plt
+import seaborn
+
 
 def read():
     df = pd.read_csv("data/mbti.csv")
@@ -101,7 +106,27 @@ def read():
 
     mbti = dict(zip(list(unique_types), posts_by_type))
     for key, value in mbti.items():
-        print(key, ':', len(value))
+        print(key, ':', value[0])
 
     return mbti
     # ----------------------------------------------------------------
+
+
+types = ['INFP', 'ESFJ', 'ESFP', 'ISFJ', 'ISFP', 'ESTJ', 'ESTP', 'ISTJ', 'ISTP', 'INTJ', 'INFJ', 'ENTJ', 'ENTP', 'ENFJ',
+         'INTP', 'ENFP']
+
+
+def read_new():
+    df = pd.read_csv("data/mbti.csv")
+
+    plt.figure()
+    plt.xticks(fontsize=8)
+    seaborn.countplot(data=df, x='type')
+    plt.show()
+
+    data = dict([(t, []) for t in types])
+
+    for i, row in df.iterrows():
+        data[row.type].extend(row.posts.split('|||'))
+
+    return data
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import nltk
 import sklearn
 import re
-from data_loading import read
+from data_loading import read, read_new
+
+from nltk.stem import WordNetLemmatizer;
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+
 
 def posts_cleaning(dict):
 
@@ -15,10 +20,22 @@ def posts_cleaning(dict):
 
     return dict
 
+
+def get_processed_words_for_type(sentence, lemmatizer, stop_words_corpus):
+    words = word_tokenize(sentence)
+    words = [lemmatizer.lemmatize(w) for w in words]
+    words = [w for w in words if w not in stop_words_corpus]
+    return words
+
+
+def get_bag_of_words(words):
+    return dict([(w, True) for w in words])
+
+
 if __name__ == '__main__':
 
-    dict = read()
-    #cleaned_dict = posts_cleaning(dict)
+    data = read_new()
+    #cleaned_dict = posts_cleaning(data)
 
     post = "Do I hate it when people find me attractive? Hell no. k http://www.politicalcompass.org/facebook/pcgraphpng.php?ec=3.75&soc=-4.72 I wish people did."
     print(post)
@@ -28,3 +45,18 @@ if __name__ == '__main__':
     post = re.sub("[^a-zA-Z]", " ", post)
     post = re.sub(' +', ' ', post).lower()
     print(post)
+
+    lemmatizer = WordNetLemmatizer()
+    eng_stopwords = set(stopwords.words('english'))
+
+    words_for_type = {}
+    for key, value in data.items():
+        for sentence in value:
+            tokenized = get_processed_words_for_type(sentence, lemmatizer, eng_stopwords)
+            bag = get_bag_of_words(tokenized)
+            if key in words_for_type:
+                words_for_type[key].update(bag)
+            else:
+                words_for_type[key] = bag
+
+
