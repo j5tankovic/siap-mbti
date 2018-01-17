69859784c1d230516e2dbf6bf6368ccec973dd31
unknown <ivana.unitedforce@gmail.com>
2018-01-17 09:07:24 +0100

    obrisan visak

--- main.py
+++ main.py
@@ -35,22 +35,13 @@ def get_bag_of_words(words):
 if __name__ == '__main__':
 
     data = read_new()
-    #cleaned_dict = posts_cleaning(data)
-
-    post = "Do I hate it when people find me attractive? Hell no. k http://www.politicalcompass.org/facebook/pcgraphpng.php?ec=3.75&soc=-4.72 I wish people did."
-    print(post)
-
-    post = re.sub("[.,:?!+]", " ", post)
-    post = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', 'link', post)
-    post = re.sub("[^a-zA-Z]", " ", post)
-    post = re.sub(' +', ' ', post).lower()
-    print(post)
+    cleaned_dict = posts_cleaning(data)
 
     lemmatizer = WordNetLemmatizer()
     eng_stopwords = set(stopwords.words('english'))
 
     words_for_type = {}
-    for key, value in data.items():
+    for key, value in cleaned_dict.items():
         for sentence in value:
             tokenized = get_processed_words_for_type(sentence, lemmatizer, eng_stopwords)
             bag = get_bag_of_words(tokenized)
